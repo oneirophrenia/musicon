a8a9d9724092b53171dea578c11a989d5d47c832
oneirophrenia <gentlymaestro0@gmail.com>
2025-05-27 18:13:38 +0300

    ya ne budu smotret na vashi penisi

--- app.js
+++ app.js
@@ -71,5 +71,5 @@ app.get('/logout', (req, res) => {
 // Запуск сервера
 
 app.listen(PORT, () => {
-  console.log(Server is running on http://localhost:${PORT});
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
